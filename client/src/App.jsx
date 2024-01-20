0b17ddfe7df79166344997f03dd05d0ec3a48b3b
Rishi <singhrishi2929@gmail.com>
2024-01-20 18:52:51 +0530

    initialized the Quiz interface folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,22 @@
-import './App.module.scss'
-import Analytics from './pages/Analytics';
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login'
-import styles from './styles/Login.module.scss';
+import "./App.module.scss";
+import Analytics from "./pages/Analytics";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import styles from "./styles/Login.module.scss";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
-function App() {
-
+const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/analytics' element={<Analytics />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/analytics" element={<Analytics />} />
+        <Route path="/quiz/:id" element={<Quiz />} />
       </Routes>
     </Router>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
---
+++ client/src/components/quizInterface/quiz/Quiz.jsx
@@ -0,0 +1,35 @@
+import { useParams, useNavigate } from "react-router-dom";
+
+const Quiz = () => {
+
+  const quiz = [
+    {
+      time: "",
+      id: "",
+      question: "Your question text comes here, its a sample text.",
+      options: [
+        { 
+            id: "",
+            option: "Option 1",
+            image: "",
+        },
+        { 
+            id: "",
+            option: "Option 2",
+            image: "",
+        },
+        { 
+            id: "",
+            option: "Option 3",
+            image: "",
+        },
+        { 
+            id: "",
+            option: "Option 4",
+            image: "",
+        },
+      ],
+    },
+  ];
+
+};
