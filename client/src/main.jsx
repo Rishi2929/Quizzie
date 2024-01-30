5abbf80411e9e8a49fc089450a7de707b05a709b
Rishi2929 <128122653+Rishi2929@users.noreply.github.com>
2024-01-31 00:11:54 +0530

    Deploying

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import React, { createContext, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-
 export const Context = createContext({ isAuthenticated: false })
 
 const AppWrapper = () => {
@@ -10,14 +9,10 @@ const AppWrapper = () => {
   const [loading, setLoading] = useState(false)
 
 
-  // const [user, setUser] = useState({})
-
-
   return (
     <Context.Provider value={{
       isAuthenticated, setIsAuthenticated,
       loading, setLoading,
-      // user, setUser
     }}>
       <App />
     </Context.Provider>
@@ -26,7 +21,7 @@ const AppWrapper = () => {
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <AppWrapper />
-  // </React.StrictMode> 
+  <React.StrictMode>
+    <AppWrapper />
+  </React.StrictMode>
 )
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -7,19 +7,17 @@ import { Context } from "../main";
 import moment from 'moment';
 import axios from "axios";
 import { server } from "../App";
+import CustomLoader from "../components/CurstomLoader";
 
 const Dashboard = () => {
 
-  const { setUser, setIsAuthenticated, setLoading } = useContext(Context)
+  const { setUser, setIsAuthenticated, setLoading, isAuthenticated, loading } = useContext(Context)
   const [tableData, setTableData] = useState([]);
-
-
-  // console.log(sumOfQuestions)
-  // console.log(tableData)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true)
         const token = localStorage.getItem('token');
         const response = await axios.get(`${server}/quiz/myQuiz`, {
           headers: {
@@ -31,75 +29,76 @@ const Dashboard = () => {
       } catch (error) {
         console.error('Error fetching data:', error);
       }
+      finally {
+        setLoading(false);
+      }
     };
-
-    // Call the fetchData function when the component mounts
     fetchData();
   }, []);
 
   const sumOfQuestions = tableData.reduce((sum, quiz) => sum + quiz.questions.length, 0);
 
 
-  // if (isAuthenticated) return <Navigate to={"/"} />
+  // if (!isAuthenticated) return <Navigate to={"/"} />
 
   return (
     <div className={styles["dashboard-parent-cont"]}>
       <Navbar />
-      <div className={styles["dashboard-child-cont"]}>
-
-        <div className={styles["dashboard-flex-cont"]}>
-          <div className={styles["dashboard-cont"]}>
-            <div className={styles['flex-cont-1']}>
-
-              <h1>
-                {tableData.length}<span>Quiz</span>
-              </h1>
-
-              <h1>
-                <span>Created</span>
-              </h1>
-            </div>
-          </div>
-          <div className={styles["dashboard-cont"]}>
-            <div className={styles['flex-cont-2']}>
-
-              <h1>
-                {sumOfQuestions} <span>questions</span>
-              </h1>
-
-              <h1>
-                <span>Created</span>
-              </h1>
-            </div>
-          </div>
-          <div className={styles["dashboard-cont"]}>
-            <div className={styles['flex-cont-3']}>
-              <h1>
-                12 <span>Total</span>
-              </h1>
-              <h1>
-                <span>Impressions</span>
-              </h1>
-            </div>
-          </div>
-        </div>
-        <div className={styles['quiz-parent-cont']}>
-          <h2>Trending Quizs</h2>
-          <div className={styles['quiz-cont']}>
-            {tableData.map((quiz, index) => (
-              <div className={styles['quiz-box']} key={index}>
-                <div className={styles['quiz-flex-data']}>
-                  <p className={styles['quiz-name']}>{quiz.quizName}</p>
-                  <p> {quiz.quizCount}<img src={views} alt="" /></p>
-                </div>
-                <p className={styles['green-span']}>Created on: {moment(quiz.createdAt).format('DD MMM, YYYY')}</p>
+      {loading ? (
+        <CustomLoader />
+      ) : (
+        <div className={styles["dashboard-child-cont"]}>
+          <div className={styles["dashboard-flex-cont"]}>
+            <div className={styles["dashboard-cont"]}>
+              <div className={styles['flex-cont-1']}>
+                <h1>
+                  {tableData.length}<span>Quiz</span>
+                </h1>
+                <h1>
+                  <span>Created</span>
+                </h1>
               </div>
-            ))}
+            </div>
+            <div className={styles["dashboard-cont"]}>
+              <div className={styles['flex-cont-2']}>
+                <h1>
+                  {sumOfQuestions} <span>questions</span>
+                </h1>
+                <h1>
+                  <span>Created</span>
+                </h1>
+              </div>
+            </div>
+            <div className={styles["dashboard-cont"]}>
+              <div className={styles['flex-cont-3']}>
+                <h1>
+                  12 <span>Total</span>
+                </h1>
+                <h1>
+                  <span>Impressions</span>
+                </h1>
+              </div>
+            </div>
+          </div>
+          <div className={styles['quiz-parent-cont']}>
+            <h2>Trending Quizs</h2>
+            <div className={styles['quiz-cont']}>
+              {tableData.map((quiz, index) => (
+                <div className={styles['quiz-box']} key={index}>
+                  <div className={styles['quiz-flex-data']}>
+                    <p className={styles['quiz-name']}>{quiz.quizName}</p>
+                    <p> {quiz.quizCount}<img src={views} alt="" /></p>
+                  </div>
+                  <p className={styles['green-span']}>Created on: {moment(quiz.createdAt).format('DD MMM, YYYY')}</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
+
 };
 
 export default Dashboard;
